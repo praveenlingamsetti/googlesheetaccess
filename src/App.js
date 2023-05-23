62e52994eb72ec8d18e2c6c77f9d48507cc14015
praveenlingamsetti <praveenlingamsetti333@gmail.com>
2023-05-23 12:36:12 +0530

    googlesheetaccess

---
+++ src/App.js
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from "react";
+
+import { gapi } from "gapi-script";
+
+const SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+
+const ApiKeyTest = () => {
+  const [sheetData, setSheetData] = useState([]);
+  const [userEmail, setUserEmail] = useState("");
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  useEffect(() => {
+    const loadGoogleAPI = () => {
+      const script = document.createElement("script");
+      script.src = "https://apis.google.com/js/api.js";
+      script.onload = initializeGoogleAPI;
+      document.head.appendChild(script);
+    };
+
+    const initializeGoogleAPI = () => {
+      window.gapi.load("client:auth2", initClient);
+    };
+
+    const initClient = () => {
+      window.gapi.client
+        .init({
+          apiKey: "AIzaSyAz1z7QqYvovxmnO-lvzoORcMC1UZzXNRE",
+          clientId:
+            "183989250797-3891nl9n8vj69r8ju7lmh3s5n307ma04.apps.googleusercontent.com",
+          scope: SCOPES,
+          discoveryDocs: [
+            "https://sheets.googleapis.com/$discovery/rest?version=v4",
+          ],
+        })
+        .then(() => {
+          console.log("Google API client initialized");
+          const authInstance = window.gapi.auth2.getAuthInstance();
+          setIsSignedIn(authInstance.isSignedIn.get());
+          authInstance.isSignedIn.listen(updateSignInStatus);
+          executeRequest();
+          getUserEmail();
+        })
+        .catch((error) => {
+          console.error("Error initializing Google API client", error);
+        });
+    };
+
+    const updateSignInStatus = (isUserSignedIn) => {
+      setIsSignedIn(isUserSignedIn);
+      if (isUserSignedIn) {
+        getUserEmail();
+      } else {
+        setUserEmail("");
+      }
+    };
+
+    const executeRequest = () => {
+      if (isSignedIn) return; // Don't execute if user is not signed in
+      window.gapi.client.sheets.spreadsheets.values
+        .get({
+          spreadsheetId: "1SdZNPvV7wHHlVMG0OkiBwnf4UVxHHI6hlXqeH-04T6o",
+          range: "sheet",
+        })
+        .then((response) => {
+          setSheetData(response.result.values || []);
+        })
+        .catch((error) => {
+          console.error("Error executing request", error);
+        });
+    };
+
+    const getUserEmail = () => {
+      const authInstance = window.gapi.auth2.getAuthInstance();
+      if (authInstance.isSignedIn.get()) {
+        const currentUser = authInstance.currentUser.get();
+        const basicProfile = currentUser.getBasicProfile();
+        const email = basicProfile.getEmail();
+
+        setUserEmail(email);
+      }
+    };
+
+    loadGoogleAPI();
+  }, []);
+
+  const handleSignIn = () => {
+    const authInstance = window.gapi.auth2.getAuthInstance();
+    authInstance.signIn();
+  };
+
+  const handleSignOut = () => {
+    const authInstance = window.gapi.auth2.getAuthInstance();
+    authInstance.signOut();
+  };
+
+  return (
+    <div>
+      {console.log(sheetData)}
+      <h2>Google Sheet API Example</h2>
+      <p>
+        {isSignedIn ? (
+          <span>
+            Logged in as: {userEmail} |{" "}
+            <button onClick={handleSignOut}>Sign Out</button>
+          </span>
+        ) : (
+          <button onClick={handleSignIn}>Sign In with Google</button>
+        )}
+      </p>
+      {isSignedIn && (
+        <ul>
+          {sheetData.map((row, index) => (
+            <li key={index}>{row.join(" | ")}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default ApiKeyTest;
